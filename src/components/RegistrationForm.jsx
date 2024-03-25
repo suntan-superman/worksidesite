4591b4ebf3877f6b29c4c2fbb3738b659bdec72d
suntan-superman <sjroy5@hotmail.com>
2024-03-25 13:38:25 -0700

    2024-03-25

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Select from "react-select";
-import { Link } from "react-router-dom";
 
 const options = [
   { value: "SUPPLIER", label: "Supplier" },
